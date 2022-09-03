a6631b7f4fa7b04024cb222e15846b6d364f525e
Saksham Kumar <shkumar@gainsight.com>
2022-09-03 19:12:42 +0530

    Implemented Spring for loose coupling

--- src/main/java/com/example/saksham/SpringWalkthrough/sortAlgorithm/BubbleSort.java
+++ src/main/java/com/example/saksham/SpringWalkthrough/sortAlgorithm/BubbleSort.java
@@ -1,5 +1,10 @@
 package com.example.saksham.SpringWalkthrough.sortAlgorithm;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+
 public class BubbleSort implements SortInterface {
     public int[] sortNumbers(int[] numbers) {
         return  numbers;
--- src/main/java/com/example/saksham/SpringWalkthrough/sortAlgorithm/QuickSort.java
+++ src/main/java/com/example/saksham/SpringWalkthrough/sortAlgorithm/QuickSort.java
@@ -1,7 +1,12 @@
 package com.example.saksham.SpringWalkthrough.sortAlgorithm;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class QuickSort implements SortInterface {
+
     public int[] sortNumbers(int[] numbers) {
         return  numbers;
     }
+
 }
