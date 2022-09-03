922df95772b75507bc394e4a4e8fb1dda8c82e0c
Saksham Kumar <shkumar@gainsight.com>
2022-09-03 19:12:42 +0530

    Implemented Spring for loose coupling

--- src/main/java/com/example/saksham/SpringWalkthrough/SpringWalkthroughApplication.java
+++ src/main/java/com/example/saksham/SpringWalkthrough/SpringWalkthroughApplication.java
@@ -1,18 +1,18 @@
 package com.example.saksham.SpringWalkthrough;
 
-import com.example.saksham.SpringWalkthrough.sortAlgorithm.BubbleSort;
-import com.example.saksham.SpringWalkthrough.sortAlgorithm.MergeSort;
 import com.example.saksham.SpringWalkthrough.sortAlgorithm.QuickSort;
 import com.example.saksham.SpringWalkthrough.util.BinarySearchImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringWalkthroughApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringWalkthroughApplication.class, args);
-		int result = new BinarySearchImpl(new QuickSort()).binarySearchImpl(new int[] {}, 3);
+		ApplicationContext applicationContext = SpringApplication.run(SpringWalkthroughApplication.class, args);
+		BinarySearchImpl binarySearchImpl = applicationContext.getBean(BinarySearchImpl.class);
+		int result = binarySearchImpl.binarySearch(new int[] {}, 3);
 		System.out.println(result);
 	}
 
--- src/main/java/com/example/saksham/SpringWalkthrough/util/BinarySearchImpl.java
+++ src/main/java/com/example/saksham/SpringWalkthrough/util/BinarySearchImpl.java
@@ -2,17 +2,16 @@ package com.example.saksham.SpringWalkthrough.util;
 
 import com.example.saksham.SpringWalkthrough.sortAlgorithm.BubbleSort;
 import com.example.saksham.SpringWalkthrough.sortAlgorithm.SortInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class BinarySearchImpl {
 
+    @Autowired
     private SortInterface sortInterface;
 
-    public BinarySearchImpl(SortInterface sortInterface) {
-        this.sortInterface = sortInterface;
-
-    }
-
-    public int binarySearchImpl(int[] numbers, int num) {
+    public int binarySearch(int[] numbers, int num) {
 
         numbers = sortInterface.sortNumbers(numbers);
 
